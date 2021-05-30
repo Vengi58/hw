0016cda9dd68694a59af905d3bea8c83883dbf3a
Tamas <tamas.veingartner@r3.com>
2021-05-30 20:33:30 +0100

    rename test file and class

--- PromotionEngine/PromotionEngineTests/PromotionRuleExtensionTests.cs
+++ PromotionEngine/PromotionEngineTests/PromotionRuleExtensionTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace PromotionEngineTests
 {
-    public class PromotionExtensionTests
+    public class PromotionRuleExtensionTests
     {
         [Test]
         public void CombinedItemFixedPricePromotionParse()
